91391872a9994c072dd60241c5c5002a1d078c96
Dilshan <dopatha@nc.rr.com>
2020-08-17 21:07:17 -0400

    final pushes

--- app.js
+++ app.js
@@ -9,27 +9,175 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const employeeTeam = [];
+const ids = [];
 
+const valueValidator = answer => {
+    if (answer === "") {
+        return "You need to provide an answer";
+    } return true;
+}
+const idValidator = answer => {  
+    const newId = answer.trim();
+    if (ids.includes(newId)) {
+        return "Id already taken";
+    }else if (answer === "") {
+        return "You need to provide an answer";
+    } 
+    else {
+        ids.push(newId);
+        return true;
+    } 
+}
+const emailValidator= answer=>{
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(answer)) {
+        return true;
+    }
+    return "Please enter a valid email address.";
+}
+const managerQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is your manager's name?:",
+        validate: valueValidator
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is your manager's id?:",
+        validate: idValidator
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your manager's email?:",
+        validate: emailValidator
+    },
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "What is your manager's office number:",
+        validate: valueValidator
+    }
+]
+const engineerQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is your engineer's name?:",
+        validate: valueValidator
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is your engineer's id?:",
+        validate: idValidator
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your engineer's email?:",
+        validate: emailValidator
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is your engineer's GitHub username?:",
+        validate: valueValidator
+    }
+]
+const internQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is your intern's name?:",
+        validate: valueValidator
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is your intern's id?:",
+        validate: idValidator
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your intern's email?:",
+        validate: emailValidator
+    },
+    {
+        type: "input",
+        name: "school",
+        message: "What is your  intern's school?:",
+        validate: valueValidator
+    }
+]
+const moreEmployeeQuestions = [
+    {
+        type: "list",
+        name: "options",
+        message: "Which type of team member would you like to add?",
+        choices: [
+            "Engineer",
+            "Intern",
+            "I don't want to add anymore members"
+        ]
+    }
+]
+async function promptMoreUser() {
+    try {
+        const answers = await inquirer.prompt(moreEmployeeQuestions);
+        if (answers.options === "Engineer") {
+            createEngineer();
+        } else if (answers.options === "Intern") {
+            createIntern();
+        } else {
+            const teamString = render(employeeTeam);
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+            fs.writeFile(outputPath, teamString, function (err) {
+                if (err) {
+                    throw err
+                };
+            });
+        }
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+async function createEngineer() {
+    try {
+        const answers = await inquirer.prompt(engineerQuestions);
+        const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+        employeeTeam.push(engineer);
+        promptMoreUser();
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+async function createIntern() {
+    try {
+        const answers = await inquirer.prompt(internQuestions);
+        const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+        employeeTeam.push(intern);
+        promptMoreUser();
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+async function init() {
+    try {
+        console.log("Please Build your team");
+        const answers = await inquirer.prompt(managerQuestions);
+        const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+        employeeTeam.push(manager);
+        promptMoreUser();
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+init();
